a6ad601a92ba06cd8f214c9369e08d4eb3f4f5aa
aria1th <35677394+aria1th@users.noreply.github.com>
2022-08-30 01:36:22 +0900

    add renewableCalcite

--- src/main/java/de/rubixdev/rug/RugSettings.java
+++ src/main/java/de/rubixdev/rug/RugSettings.java
@@ -159,7 +159,16 @@ public class RugSettings {
         @Override
         public Double validate(
                 ServerCommandSource source, CarpetRule<Double> currentRule, Double newValue, String string) {
-            return newValue >= 0 && newValue <= (CarpetServer.minecraft_server == null ? 100 : CarpetServer.minecraft_server.getPlayerManager().getSimulationDistance() * 16) ? newValue : null;
+            return newValue >= 0
+                            && newValue
+                                    <= (CarpetServer.minecraft_server == null
+                                            ? 100
+                                            : CarpetServer.minecraft_server
+                                                            .getPlayerManager()
+                                                            .getSimulationDistance()
+                                                    * 16)
+                    ? newValue
+                    : null;
         }
 
         @Override
@@ -712,6 +721,9 @@ public class RugSettings {
     @Rule(categories = {EXPERIMENTAL, FEATURE, SURVIVAL, RENEWABLE, RUG})
     public static boolean basaltToBlackstoneConversion = false;
 
+    @Rule(categories = {FEATURE, SURVIVAL, RENEWABLE, RUG})
+    public static boolean renewableCalcite = false;
+
     @Rule(categories = {SURVIVAL, RENEWABLE, RUG})
     public static boolean infiniteLavaSources = false;
 
@@ -720,6 +732,11 @@ public class RugSettings {
 
     @Rule(categories = {COMMAND, RUG})
     public static String commandSudo = "ops";
+
+    @Rule(
+            options = {"0", "1", "2", "3"},
+            categories = {EXPERIMENTAL, RUG})
+    public static int reachPermissionLevel = 0;
 
     @Rule(categories = {RUG})
     public static boolean endCrystalPlacementRestriction = true;
