1211858c3a332f4df14c5180d15343a1d137d042
aria1th <35677394+aria1th@users.noreply.github.com>
2022-08-30 00:12:29 +0900

    Safer & Validify Reach distance

--- src/main/java/de/rubixdev/rug/mixins/reach/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/de/rubixdev/rug/mixins/reach/ServerPlayNetworkHandlerMixin.java
@@ -1,8 +1,9 @@
 package de.rubixdev.rug.mixins.reach;
 
-
 import de.rubixdev.rug.RugSettings;
+import net.minecraft.block.BlockState;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,31 +17,47 @@ public class ServerPlayNetworkHandlerMixin {
     @Shadow
     @Final
     public static double MAX_BREAK_SQUARED_DISTANCE;
+	@Shadow public ServerPlayerEntity player;
 
-    @Redirect(
-        method = "onPlayerInteractEntity",
-        at = @At(
-            value = "FIELD",
-            target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;MAX_BREAK_SQUARED_DISTANCE:D"
-        )
-    )
+	@Redirect(
+            method = "onPlayerInteractEntity",
+            at =
+                    @At(
+                            value = "FIELD",
+                            target =
+                                    "Lnet/minecraft/server/network/ServerPlayNetworkHandler;MAX_BREAK_SQUARED_DISTANCE:D"))
     private double changeAttackRangeForEntities() {
-        return Math.pow(Math.sqrt(MAX_BREAK_SQUARED_DISTANCE) + RugSettings.reachDistance - 4.5, 2);
+	    if (this.player == null){
+		    return ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE;
+	    }
+	    if (this.player.hasPermissionLevel(2))
+		    return Math.pow(Math.sqrt(ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE) + RugSettings.reachDistance - 4.5, 2);
+	    return ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE;
     }
 
     @Redirect(
-        method = "onPlayerInteractBlock",
-        at = @At(
-            value = "FIELD",
-            target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;MAX_BREAK_SQUARED_DISTANCE:D"
-        )
-    )
+            method = "onPlayerInteractBlock",
+            at =
+                    @At(
+                            value = "FIELD",
+                            target =
+                                    "Lnet/minecraft/server/network/ServerPlayNetworkHandler;MAX_BREAK_SQUARED_DISTANCE:D"))
     private double changeAttackRangeForBlocks() {
-        return Math.pow(Math.sqrt(MAX_BREAK_SQUARED_DISTANCE) + RugSettings.reachDistance - 4.5, 2);
+	    if (player == null){
+		    return MAX_BREAK_SQUARED_DISTANCE;
+	    }
+	    if (player.hasPermissionLevel(2))
+		    return Math.pow(Math.sqrt(MAX_BREAK_SQUARED_DISTANCE) + RugSettings.reachDistance - 4.5, 2);
+	    return MAX_BREAK_SQUARED_DISTANCE;
     }
 
     @ModifyConstant(method = "onPlayerInteractBlock", allow = 1, require = 1, constant = @Constant(doubleValue = 64.0))
     private double changeReachDistance(final double baseReachDistance) {
-        return Math.pow(Math.sqrt(baseReachDistance) + RugSettings.reachDistance - 4.5, 2);
+	    if (player == null){
+		    return MAX_BREAK_SQUARED_DISTANCE;
+	    }
+	    if (player.hasPermissionLevel(2))
+		    return Math.pow(Math.sqrt(MAX_BREAK_SQUARED_DISTANCE) + RugSettings.reachDistance - 4.5, 2);
+	    return MAX_BREAK_SQUARED_DISTANCE;
     }
 }
--- src/main/java/de/rubixdev/rug/mixins/reach/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/de/rubixdev/rug/mixins/reach/ServerPlayerInteractionManagerMixin.java
@@ -1,26 +1,33 @@
 package de.rubixdev.rug.mixins.reach;
 
-
 import de.rubixdev.rug.RugSettings;
+import net.minecraft.block.BlockState;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerPlayerInteractionManager.class)
 public class ServerPlayerInteractionManagerMixin {
-    @Redirect(
-        method = "processBlockBreakingAction",
-        at = @At(
-            value = "FIELD",
-            target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;MAX_BREAK_SQUARED_DISTANCE:D"
-        )
-    )
+	@Shadow @Final protected ServerPlayerEntity player;
+
+	@Redirect(
+            method = "processBlockBreakingAction",
+            at =
+                    @At(
+                            value = "FIELD",
+                            target =
+                                    "Lnet/minecraft/server/network/ServerPlayNetworkHandler;MAX_BREAK_SQUARED_DISTANCE:D"))
     private double changeReachDistance() {
-        return Math.pow(
-            Math.sqrt(ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE) + RugSettings.reachDistance - 4.5,
-            2
-        );
+	    if (this.player == null){
+		    return ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE;
+	    }
+	    if (this.player.hasPermissionLevel(2))
+		    return Math.pow(Math.sqrt(ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE) + RugSettings.reachDistance - 4.5, 2);
+	    return ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE;
     }
 }
