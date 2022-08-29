01253d7af9218fbef452653fb27f1814f5bb1ee4
aria1th <35677394+aria1th@users.noreply.github.com>
2022-08-30 00:12:29 +0900

    Safer & Validify Reach distance

--- src/main/java/de/rubixdev/rug/mixins/reach/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/de/rubixdev/rug/mixins/reach/ServerPlayerInteractionManagerMixin.java
@@ -1,15 +1,21 @@
 package de.rubixdev.rug.mixins.reach;
 
 import de.rubixdev.rug.RugSettings;
+import net.minecraft.block.BlockState;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerPlayerInteractionManager.class)
 public class ServerPlayerInteractionManagerMixin {
-    @Redirect(
+	@Shadow @Final protected ServerPlayerEntity player;
+
+	@Redirect(
             method = "processBlockBreakingAction",
             at =
                     @At(
@@ -17,7 +23,11 @@ public class ServerPlayerInteractionManagerMixin {
                             target =
                                     "Lnet/minecraft/server/network/ServerPlayNetworkHandler;MAX_BREAK_SQUARED_DISTANCE:D"))
     private double changeReachDistance() {
-        return Math.pow(
-                Math.sqrt(ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE) + RugSettings.reachDistance - 4.5, 2);
+	    if (this.player == null){
+		    return ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE;
+	    }
+	    if (this.player.hasPermissionLevel(2))
+		    return Math.pow(Math.sqrt(ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE) + RugSettings.reachDistance - 4.5, 2);
+	    return ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE;
     }
 }
